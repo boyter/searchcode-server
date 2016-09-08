f3ff5d76b48da3b06200f526da7c02180ece56c1
Ben Boyter <ben@boyter.org>
2016-09-08 18:04:57 +1000

    Modify time code test to be integration and check index properly

--- src/test/java/com/searchcode/app/service/TimeCodeSearcherTest.java
+++ src/test/java/com/searchcode/app/service/TimeCodeSearcherTest.java
@@ -1,20 +1,37 @@
 package com.searchcode.app.service;
 
+import com.searchcode.app.dto.CodeIndexDocument;
 import com.searchcode.app.dto.CodeResult;
 import junit.framework.TestCase;
+import org.eclipse.jetty.util.ConcurrentArrayQueue;
+
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimeCodeSearcherTest extends TestCase {
 
-    // TODO fix tests in here so they do more than just call methods to ensure no exceptions thrown
-    // probably need to be more of an integration test where we write to the index then read back
-    public void testIndex() {
-        TimeCodeSearcher cs = new TimeCodeSearcher();
-        cs.search("this", 0);
-        cs.search("code", 0);
-        cs.search("should", 0);
+    public void testTimeIndexIndexCreationAndSearch() {
+        try {
+            String contents = "this is some code that should be found";
 
-        for(int i=0; i<100;i++) {
-            CodeResult cr = cs.getById(i);
+            CodeIndexDocument cid = new CodeIndexDocument("repoLocationRepoNameLocationFilename", "", "fileName", "fileLocation", "fileLocationFilename", "md5hash", "languageName", 0, contents, "repoRemoteLocation", "codeOwner");
+            cid.setRevision("99a5a271063def87b2473be79ce6f840d42d1f95");
+            cid.setYearMonthDay("20160101");
+
+            Queue queue = new ConcurrentArrayQueue<CodeIndexDocument>();
+            queue.add(cid);
+
+            CodeIndexer.indexTimeDocuments(queue);
+
+            TimeCodeSearcher cs = new TimeCodeSearcher();
+
+            assertThat(cs.search("this", 0).getCodeResultList()).hasAtLeastOneElementOfType(CodeResult.class);
+            assertThat(cs.search("code should", 0).getCodeResultList()).hasAtLeastOneElementOfType(CodeResult.class);
+            assertThat(cs.search("should be found", 0).getCodeResultList()).hasAtLeastOneElementOfType(CodeResult.class);
+        }
+        catch(Exception ex) {
+            assertTrue(false);
         }
     }
 }
