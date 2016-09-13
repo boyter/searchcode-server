3e27b1591323028a817d0df3b84d260572b25211
Ben Boyter <ben@boyter.org>
2016-09-13 18:05:01 +1000

    Add new DTO for indexing git time data

--- src/main/java/com/searchcode/app/jobs/IndexGitHistoryJob.java
+++ src/main/java/com/searchcode/app/jobs/IndexGitHistoryJob.java
@@ -10,31 +10,19 @@ package com.searchcode.app.jobs;
 import com.google.common.collect.Lists;
 import com.searchcode.app.config.Values;
 import com.searchcode.app.dto.CodeIndexDocument;
-import com.searchcode.app.dto.RepositoryChanged;
 import com.searchcode.app.service.CodeIndexer;
 import com.searchcode.app.service.GitService;
 import com.searchcode.app.service.Singleton;
 import com.searchcode.app.util.Helpers;
-import org.apache.commons.io.FilenameUtils;
-import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.errors.IncorrectObjectTypeException;
-import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevTree;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
-import org.eclipse.jgit.treewalk.TreeWalk;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -42,7 +30,7 @@ import org.quartz.JobExecutionException;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.InvalidPathException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -77,29 +65,30 @@ public class IndexGitHistoryJob implements Job {
         Git git = new Git(localRepository);
         Iterable<RevCommit> logs = git.log().call();
 
-        List<String> revisions = new ArrayList<>();
+        List<GitChangeSet> gitChangeSets = new ArrayList<>();
         for(RevCommit rev: logs) {
             String message = rev.getFullMessage();
             String author = rev.getAuthorIdent().getName();
 
-            Date expiry = new Date(new Long(rev.getCommitTime()) * 1000); // TODO need the time in here as well
+            Date expiry = new Date(new Long(rev.getCommitTime()) * 1000);
             System.out.println(expiry.toString() + " " + rev.getCommitTime() + " " + rev.getName());
-            revisions.add(rev.getName());
 
+            gitChangeSets.add(new GitChangeSet(message, author, rev.getName(), expiry));
         }
-        revisions = Lists.reverse(revisions);
+
+        gitChangeSets = Lists.reverse(gitChangeSets);
 
         // TODO currently this is ignoring the very first commit changes need to include those
-        for (int i = 1; i < revisions.size(); i++) {
+        for (int i = 1; i < gitChangeSets.size(); i++) {
             System.out.println("///////////////////////////////////////////////");
-            this.getRevisionChanges(localRepository, git, revisions.get(i - 1), revisions.get(i));
+            this.getRevisionChanges(localRepository, git, gitChangeSets.get(i - 1), gitChangeSets.get(i));
         }
 
     }
 
-    public void getRevisionChanges(Repository localRepository, Git git, String oldRevison, String newRevision) throws IOException, GitAPIException {
-        ObjectId oldHead = localRepository.resolve(oldRevison + "^{tree}");
-        ObjectId newHead = localRepository.resolve(newRevision + "^{tree}");
+    public void getRevisionChanges(Repository localRepository, Git git, GitChangeSet oldRevison, GitChangeSet newRevision) throws IOException, GitAPIException {
+        ObjectId oldHead = localRepository.resolve(oldRevison.getRevision() + "^{tree}");
+        ObjectId newHead = localRepository.resolve(newRevision.getRevision() + "^{tree}");
 
         ObjectReader reader = localRepository.newObjectReader();
 
@@ -116,33 +105,78 @@ public class IndexGitHistoryJob implements Job {
                 .call();
 
         GitService gs = new GitService();
-
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 
         for( DiffEntry entry : entries ) {
             if ("DELETE".equals(entry.getChangeType().name())) {
                 System.out.println("DEL " + entry.getOldPath());
 
-                String contents = gs.fetchFileRevision(localRepository.getWorkTree().toString() + "/.git", oldRevison, entry.getOldPath());
+                String contents = gs.fetchFileRevision(localRepository.getWorkTree().toString() + "/.git", oldRevison.getRevision(), entry.getOldPath());
 
-                CodeIndexDocument cd = new CodeIndexDocument(entry.getNewPath(), "server", entry.getOldPath(), entry.getOldPath(), entry.getOldPath(), "md5hash", "Java", contents.split("\\r?\\n").length, contents, "", "someone");
-                cd.setRevision(oldRevison);
-                cd.setYearMonthDay("20160101");
-                cd.setMessage("Something something something");
+                CodeIndexDocument cd = new CodeIndexDocument(entry.getNewPath(), "server", entry.getOldPath(), entry.getOldPath(), entry.getOldPath(), "md5hash", "Java", contents.split("\\r?\\n").length, contents, "", oldRevison.getAuthor());
+                cd.setRevision(oldRevison.getRevision());
+                cd.setYearMonthDay(sdf.format(oldRevison.getExpiry()));
+                cd.setMessage(oldRevison.getMessage());
                 cd.setDeleted("TRUE");
                 CodeIndexer.indexTimeDocument(cd);
             }
             else {
                 System.out.println("ADD " + entry.getNewPath());
-                String contents = gs.fetchFileRevision(localRepository.getWorkTree().toString() + "/.git", newRevision, entry.getNewPath());
+                String contents = gs.fetchFileRevision(localRepository.getWorkTree().toString() + "/.git", newRevision.getRevision(), entry.getNewPath());
 
-                CodeIndexDocument cd = new CodeIndexDocument(entry.getNewPath(), "server", entry.getNewPath(), entry.getNewPath(), entry.getNewPath(), "md5hash", "Java", contents.split("\\r?\\n").length, contents, "", "someone");
-                cd.setRevision(newRevision);
-                cd.setYearMonthDay("20160101");
-                cd.setMessage("Something something something");
+                CodeIndexDocument cd = new CodeIndexDocument(entry.getNewPath(), "server", entry.getNewPath(), entry.getNewPath(), entry.getNewPath(), "md5hash", "Java", contents.split("\\r?\\n").length, contents, "", newRevision.getAuthor());
+                cd.setRevision(newRevision.getRevision());
+                cd.setYearMonthDay(sdf.format(oldRevison.getExpiry()));
+                cd.setMessage(newRevision.getMessage());
                 cd.setDeleted("FALSE");
                 CodeIndexer.indexTimeDocument(cd);
             }
         }
     }
 
+    public class GitChangeSet {
+        private String message;
+        private String author;
+        private Date expiry;
+        private String revision;
+
+        public GitChangeSet(String message, String author, String revision, Date expiry) {
+            this.setMessage(message);
+            this.setAuthor(author);
+            this.setRevision(revision);
+            this.setExpiry(expiry);
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public Date getExpiry() {
+            return expiry;
+        }
+
+        public void setExpiry(Date expiry) {
+            this.expiry = expiry;
+        }
+
+        public String getRevision() {
+            return revision;
+        }
+
+        public void setRevision(String revision) {
+            this.revision = revision;
+        }
+    }
 }
