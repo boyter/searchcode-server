a0671f3d70801e584a2292b4dfdf00c6f4004d56
Ben Boyter <ben@boyter.org>
2016-09-22 08:33:46 +1000

    Increment version for release

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.0
+ * Version 1.3.1
  */
 '''
 
@@ -28,7 +28,7 @@ import hashlib
 import sys
 import datetime
 
-VERSION = "1.3.0"
+VERSION = "1.3.1"
 
 def run_proxy():
     local('python ./assets/javascript_proxy/webserver.py')
