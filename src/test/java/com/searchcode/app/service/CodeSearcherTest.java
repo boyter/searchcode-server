be214584de5ce044a31db0b45d917525d26dfa6a
Ben Boyter <ben@boyter.org>
2016-10-31 17:58:31 +1100

    Update

--- src/test/java/com/searchcode/app/service/CodeSearcherTest.java
+++ src/test/java/com/searchcode/app/service/CodeSearcherTest.java
@@ -43,8 +43,6 @@ public class CodeSearcherTest extends TestCase {
 
         CodeResult cr1 = cs.getByRepoFileName("authoring", "src/app/Ninemsn.Portal.SitecoreItems.Master/sitecore/media library/Files/MediaService/angrywhale.item");
         CodeResult cr2 = cs.getByRepoFileName("authoring", "src/tests/acceptance/Ninemsn.Portal.News.Authoring.Tests.Acceptance/Steps/ImagePickerStepDefintions.cs");
-
-        cr1 = null;
     }
 
     // Integration Test
