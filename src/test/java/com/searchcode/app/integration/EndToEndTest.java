53cc7d9bd2bd3b85375f66902fde3679c58d7575
Ben Boyter <ben@boyter.org>
2016-11-17 08:55:35 +1100

    Improve end to end test

--- src/test/java/com/searchcode/app/integration/EndToEndTest.java
+++ src/test/java/com/searchcode/app/integration/EndToEndTest.java
@@ -1,5 +1,6 @@
 package com.searchcode.app.integration;
 
+import com.searchcode.app.dto.CodeResult;
 import com.searchcode.app.dto.SearchResult;
 import com.searchcode.app.jobs.IndexFileRepoJob;
 import com.searchcode.app.service.CodeIndexer;
@@ -26,26 +27,41 @@ public class EndToEndTest extends TestCase{
         SearchResult searchResult = cs.search("endtoendtestfile", 0);
         assertThat(searchResult.getCodeResultList().size()).isEqualTo(3);
 
+        CodeResult codeResult1 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile1.php")).findFirst().get();
+        CodeResult codeResult2 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile2.py")).findFirst().get();
+        CodeResult codeResult3 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile3.java")).findFirst().get();
+        assertThat(codeResult1.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile1");
+        assertThat(codeResult2.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile2");
+        assertThat(codeResult3.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile3");
+
         // Delete a single file
         String codeId = searchResult.getCodeResultList().get(0).getCodeId();
         CodeIndexer.deleteByCodeId(codeId);
         searchResult = cs.search("endtoendtestfile".toLowerCase(), 0);
         assertThat(searchResult.getCodeResultList().size()).isEqualTo(2);
 
-        // Delete file from disk then
+        codeResult1 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile2.py")).findFirst().get();
+        codeResult2 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile3.java")).findFirst().get();
+        assertThat(codeResult1.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile2");
+        assertThat(codeResult2.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile3");
+
+        // Delete file from disk then index to ensure it is removed from the index
         File toDelete = new File(directoryWithFiles.toString() + "/EndToEndTestFile2.py");
         toDelete.delete();
         indexFileRepoJob.indexDocsByPath(Paths.get(directoryWithFiles.toString()), "ENDTOENDTEST", "", directoryWithFiles.toString(), true);
         searchResult = cs.search("endtoendtestfile", 0);
         assertThat(searchResult.getCodeResultList().size()).isEqualTo(2);
 
+        codeResult1 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile1.php")).findFirst().get();
+        codeResult2 = searchResult.getCodeResultList().stream().filter(x -> x.getFileName().equals("EndToEndTestFile3.java")).findFirst().get();
+        assertThat(codeResult1.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile1");
+        assertThat(codeResult2.getCode().get(0)).isEqualTo("EndToEndTestFile EndToEndTestFile3");
+
         // Delete everything
         CodeIndexer.deleteByReponame("ENDTOENDTEST");
         searchResult = cs.search("endtoendtestfile".toLowerCase(), 0);
         assertThat(searchResult.getCodeResultList().size()).isEqualTo(0);
     }
-
-
 
     private File createDirectoryWithFiles() throws IOException {
         File tempPath = this.clearAndCreateTempPath();
