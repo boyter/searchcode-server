02697cb4f198874c23611dead43e7d7fbab67a7e
Ben Boyter <ben@boyter.org>
2016-11-18 18:17:23 +1100

    Add additional test which updates files

---
+++ assets/integration_test/fileupdatetest/fileload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Simple script that creates a file repo and adds and deletes files from it over and over till exited
+
+rm *.py
+
+while :
+do
+    now=$(date +"%T")
+
+    for i in {1..99} # this file itself counts as one
+    do
+       echo "indextest fileupdate $now" > "temp_$i.java"
+    done
+
+    echo "Sleep for 60 seconds"
+    sleep 60
+
+done
