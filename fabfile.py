1ccc139a7ed06345f34e85efdfc5859ed4a37576
Ben Boyter <ben@boyter.org>
2016-11-21 08:35:27 +1100

    New version 1.3.5 work begins

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.4
+ * Version 1.3.5
  */
 '''
 
@@ -28,7 +28,7 @@ import hashlib
 import sys
 import datetime
 
-VERSION = "1.3.4"
+VERSION = "1.3.5"
 
 
 def setup_npm():
