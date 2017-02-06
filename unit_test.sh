87301f3427a51cd4ddce1988d18f84d0227ac185
Ben Boyter <ben@boyter.org>
2017-02-07 08:31:26 +1100

    Attempt to fix pipeline

--- unit_test.sh
+++ unit_test.sh
@@ -3,7 +3,7 @@
 # If running locally we don't need to step into the directory
 if [ $# -eq 0 ]
   then
-    pushd searchcode-server 2> /dev/null || true
+    pushd searchcodeserver 2> /dev/null || true
 fi
 
 mvn test
