c323f57fade6250d1e20e2b62c7adad1fd066932
Ben Boyter <ben@boyter.org>
2017-02-13 08:17:47 +1100

    Additional tests to shake out errors in dao

--- src/test/java/com/searchcode/app/dao/DataTest.java
+++ src/test/java/com/searchcode/app/dao/DataTest.java
@@ -3,16 +3,13 @@ package com.searchcode.app.dao;
 import com.searchcode.app.config.Values;
 import com.searchcode.app.service.Singleton;
 import junit.framework.TestCase;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class DataTest extends TestCase {
-
-    public DataTest() {
-        // Tests need to bootstrap themselves
-        Data data = Singleton.getData();
-        data.createTableIfMissing();
-    }
 
     public void testDataSaveUpdate() {
         Data data = Singleton.getData();
@@ -26,13 +23,33 @@ public class DataTest extends TestCase {
         assertThat(actual).as("Checking value after saving isNew=%s, actual=%s, expected=%s", isNew, actual, expected).isEqualTo(expected);
     }
 
+    public void testGetWithoutSave() {
+        Data data = Singleton.getData();
+
+        String actual = data.getDataByName("testSingleSaveManyGet");
+        for(int i = 0; i < 200; i++) {
+            assertThat(actual).as("Get without save").isEqualTo(data.getDataByName("testSingleSaveManyGet"));
+            assertThat(actual).as("Get without save").isEqualTo(data.getDataByName("testSingleSaveManyGet", actual));
+        }
+    }
+
+    public void testGetWithRandomValuesExpectingNull() {
+        Data data = Singleton.getData();
+        Random random = new Random();
+
+        for(int i = 0; i < 200; i++) {
+            String actual = data.getDataByName(RandomStringUtils.randomAscii(random.nextInt(20) + 20));
+            assertThat(actual).isNull();
+        }
+    }
+
     public void testSingleSaveManyGet() {
         Data data = Singleton.getData();
 
         String expected = "" + System.currentTimeMillis();
         data.saveData("testSingleSaveManyGet", expected);
 
-        for(int i=0; i<200; i++) {
+        for(int i = 0; i < 200; i++) {
             assertThat(expected).as("Get with no default").isEqualTo(data.getDataByName("testSingleSaveManyGet"));
             assertThat(expected).as("Get with default").isEqualTo(data.getDataByName("testSingleSaveManyGet", "default"));
         }
