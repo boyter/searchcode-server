2dbae0202f800a21705ce25ab4dacce1525b2dc0
Ben Boyter <ben@boyter.org>
2017-03-20 09:17:35 +1100

    Increment next version

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.8
+ * Version 1.3.9
  */
 '''
 
@@ -30,7 +30,7 @@ import hashlib
 import sys
 import datetime
 
-VERSION = "1.3.8"
+VERSION = "1.3.9"
 
 
 def setup_npm():
--- src/main/java/com/searchcode/app/App.java
+++ src/main/java/com/searchcode/app/App.java
@@ -5,7 +5,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.8
+ * Version 1.3.9
  */
 
 package com.searchcode.app;
@@ -35,8 +35,8 @@ import static spark.Spark.*;
  */
 public class App {
 
-    public static final boolean ISCOMMUNITY = false;
-    public static final String VERSION = "1.3.8";
+    public static final boolean ISCOMMUNITY = true;
+    public static final String VERSION = "1.3.9";
 
     public static void main(String[] args) {
 
