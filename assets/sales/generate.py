acccba70b3733804a2a947658a5761e1e338f9d3
Ben Boyter <ben@boyter.org>
2017-04-01 14:30:55 +1100

    Static generation appears to do the trick now

--- assets/sales/generate.py
+++ assets/sales/generate.py
@@ -1,7 +1,7 @@
 # A dead simple static site generater becaues I didn't like any others
 
 import os
-from shutil import copyfile, copytree
+from shutil import copyfile, copytree, rmtree
 
 # Target output
 target = './output/'
@@ -11,10 +11,16 @@ header = './generic/header.html'
 footer = './generic/footer.html'
 
 config = [{
-        'file': 'index.html'
+        'file': 'index.html',
+        'title': 'Powerful Code Search in Your Cloud'
     }, {
-        'directory': './knowledge-base/'
-    }
+        'directory': './knowledge-base/',
+        'footer': './generic/kb_footer.html',
+        'title': 'Knowledge Base Article'
+    }, {
+        'file': './knowledge-base/index.html',
+        'title': 'The searchcode server knowledge base'
+    },
 ]
 
 assets = [
@@ -33,6 +39,12 @@ assets = [
 if not os.path.exists(target):
     os.makedirs(target)
 
+for x in os.listdir(target):
+    if os.path.isfile(target + x):
+        os.remove(target + x)
+    else:
+        rmtree(target + x)
+
 header_template = ''
 footer_template = ''
 
@@ -42,6 +54,21 @@ with open(footer, 'r') as file:
     footer_template = file.read()
 
 for conf in config:
+
+    merged_header_template = header_template
+    if conf['title']:
+        merged_header_template = header_template.replace('{{TITLE}}', conf['title'])
+
+    pre_header = ''
+    pre_footer = ''
+
+    if 'header' in conf:
+        with open(conf['header'], 'r') as myfile:
+            pre_header = myfile.read()
+    if 'footer' in conf:
+        with open(conf['footer'], 'r') as myfile:
+            pre_footer = myfile.read()
+
     if 'file' in conf:
         with open(conf['file'], 'r') as myfile:
             data = myfile.read()
@@ -49,19 +76,33 @@ for conf in config:
         if 'target' in conf:
             pass
         else:
-            with open(target + conf['file'], 'w') as myfile:
-                myfile.write(header_template + data + footer_template)
+            with open(target + conf['file'].replace('./', ''), 'w') as myfile:
+                myfile.write(merged_header_template + pre_header + data + pre_footer + footer_template)
+
     if 'directory' in conf:
-        if not os.path.exists(target):
+        if not os.path.exists(target + conf['directory']):
             os.makedirs(target + conf['directory'])
 
+        pre_header = ''
+        pre_footer = ''
+
+        if 'header' in conf:
+            with open(conf['header'], 'r') as myfile:
+                pre_header = myfile.read()
+        if 'footer' in conf:
+            with open(conf['footer'], 'r') as myfile:
+                pre_footer = myfile.read()
+
         for file in os.listdir(conf['directory']):
+
+            title = file.split('.')[0].replace('-', ' ').title()
+            merged_header_template = header_template.replace('{{TITLE}}', title)
+
             with open(conf['directory'] + file, 'r') as myfile:
                 data = myfile.read()
 
-            with open(target + conf['directory'] + file, 'w') as myfile:
-                myfile.write(header_template + data + footer_template)
-
+            with open(target + conf['directory'].replace('./', '') + file, 'w') as myfile:
+                myfile.write(merged_header_template + pre_header + data + pre_footer + footer_template)
 
 for asset in assets:
     if os.path.isdir(asset):
