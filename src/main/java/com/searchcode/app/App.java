dfa20056b3a70c8de79ebd43856d454d49115811
Ben Boyter <ben@boyter.org>
2017-04-13 08:26:57 +1000

    New version

--- src/main/java/com/searchcode/app/App.java
+++ src/main/java/com/searchcode/app/App.java
@@ -5,7 +5,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.9
+ * Version 1.3.10
  */
 
 package com.searchcode.app;
@@ -36,7 +36,7 @@ import static spark.Spark.*;
 public class App {
 
     public static final boolean ISCOMMUNITY = false;
-    public static final String VERSION = "1.3.9";
+    public static final String VERSION = "1.3.10";
 
     public static void main(String[] args) {
 
--- src/main/java/com/searchcode/app/jobs/DeleteRepositoryJob.java
+++ src/main/java/com/searchcode/app/jobs/DeleteRepositoryJob.java
@@ -5,7 +5,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.9
+ * Version 1.3.10
  */
 
 package com.searchcode.app.jobs;
