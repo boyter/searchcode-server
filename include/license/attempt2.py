78d1b49162ee4218881ceed4fe108c4709004070
Ben Boyter <ben@boyter.org>
2017-04-24 18:42:30 +1000

    Progress commit

--- include/license/attempt2.py
+++ include/license/attempt2.py
@@ -7,14 +7,51 @@ import math
 import codecs
 
 
+class VectorCompare:
+    def magnitude(self, concordance):
+        if type(concordance) != dict:
+            raise ValueError('Supplied Argument should be of type dict')
+        total = 0
+        for word, count in concordance.iteritems():
+            total += count ** 2
+        return math.sqrt(total)
+
+    def relation(self, concordance1, concordance2):
+        if type(concordance1) != dict:
+            raise ValueError('Supplied Argument 1 should be of type dict')
+        if type(concordance2) != dict:
+            raise ValueError('Supplied Argument 2 should be of type dict')
+        relevance = 0
+        topvalue = 0
+        for word, count in concordance1.iteritems():
+            if word in concordance2:
+                topvalue += count * concordance2[word]
+
+        if (self.magnitude(concordance1) * self.magnitude(concordance2)) != 0:
+            return topvalue / (self.magnitude(concordance1) * self.magnitude(concordance2))
+        else:
+            return 0
+
+    def concordance(self, document):
+        con = {}
+        for word in document.split(' '):
+            if word in con:
+                con[word] = con[word] + 1
+            else:
+                con[word] = 1
+        return con
+
+
 def load_database():
     with codecs.open('database_keywords.json', 'r', 'utf-8') as file:
         database = file.read()
 
     licenses = json.loads(database)
+    vector_compare = VectorCompare()
 
     for license in licenses:
         license['clean'] = clean_text(license['text'])
+        license['concordance'] = vector_compare.concordance(license['clean'])
 
     return licenses
 
@@ -36,7 +73,9 @@ def clean_text(text):
     return text
 
 
-def guess_license(check_license, licenses):
+def _keyword_guess(check_license, licenses):
+    matching = []
+
     for license in licenses:
         keywordmatch = 0
         for keyword in license['keywords']:
@@ -44,9 +83,33 @@ def guess_license(check_license, licenses):
                 keywordmatch = keywordmatch + 1
 
         if len(license['keywords']):
-            if (float(keywordmatch) / float(len(license['keywords'])) * 100) >= 50:
-                return license['shortname']
-    return None
+            if keywordmatch >= 1:
+                matching.append({
+                    'shortname': license['shortname'],
+                    'percentage': (float(keywordmatch) / float(len(license['keywords'])) * 100)
+                })
+
+    return matching
+
+def guess_license(check_license, licenses):
+    matching = _keyword_guess(check_license, licenses)
+
+    matches = []
+    vector_compare = VectorCompare()
+    for match in matching:
+        for license in [x for x in licenses if x['shortname'] in [y['shortname'] for y in matching]]:
+            licence_concordance = vector_compare.concordance(license['clean'])
+
+            check_license_concordance = vector_compare.concordance(check_license[:len(license['clean'])])
+
+            relation = vector_compare.relation(license['concordance'], check_license_concordance)
+
+            if relation >= 0.85:
+                matches.append((relation, license))
+
+    matches.sort(reverse=True)
+
+    return matches
 
 
 def read_clean_file(filename):
@@ -57,33 +120,26 @@ def read_clean_file(filename):
 if __name__ == '__main__':
     licenses = load_database()
 
-    project_directory = '/Users/boyter/Documents/Projects/gcc/'
+    project_directory = '/Users/boyter/Documents/Projects/searchcode-server/'
     possible_files = find_possible_licence_files(project_directory)
 
     for possible_file in possible_files:
         potential_license = read_clean_file(possible_files[0])
+        guess = guess_license(potential_license, licenses)
+        print guess[0][0], guess[0][1]['shortname'], possible_file
 
-        print guess_license(potential_license, licenses)
+    for root, dirs, files in os.walk(project_directory):
+        for file in [root + '/' + x for x in files]:
+            content = None
+            try:
+                content = read_clean_file(file)
+            except:
+                # We have issues reading binary files, just swallow the error
+                pass
 
+            if content:
+                matches = {}
+                guess = guess_license(content, licenses), file
 
-    # for root, dirs, files in os.walk(project_directory):
-    #     for file in [root + '/' + x for x in files if '.js' in x]:
-    #         matches = {}
-    #         with codecs.open(file, 'r', 'utf-8') as myfile:
-    #             content = myfile.read()
-
-    #         content = clean_text(content)
-
-    #         for license in licenses:
-    #             keywordmatch = 0
-    #             for keyword in license['keywords']:
-    #                 if keyword in content:
-    #                     keywordmatch = keywordmatch + 1
-
-    #             if len(license['keywords']):
-    #                 if (float(keywordmatch) / float(len(license['keywords'])) * 100) >= 70:
-    #                     matches[license['shortname']] = license
-
-    #         if len(matches) != 0:
-    #             for i in matches.keys():
-    #                 print i, file
+                if len(guess[0]) != 0:
+                    print guess[0][0][0], guess[0][0][1]['shortname'], file
--- include/license/test_attempt2.py
+++ include/license/test_attempt2.py
@@ -11,13 +11,19 @@ class TestIntegration(unittest.TestCase):
         license = attempt2.read_clean_file('./samples/bsd-clear')
         licenses = attempt2.load_database()
         result = attempt2.guess_license(license, licenses)
-        self.assertEqual('BSD-3-Clause-Clear', result)
+        self.assertEqual('BSD-3-Clause-Clear', result[0][1]['shortname'])
 
     def testFairSource(self):
         license = attempt2.read_clean_file('./samples/fair-source')
         licenses = attempt2.load_database()
         result = attempt2.guess_license(license, licenses)
-        self.assertEqual('Fair-Source-0.9', result)
+        self.assertEqual('Fair-Source-0.9', result[0][1]['shortname'])
+
+    def testGPLV2(self):
+        license = attempt2.read_clean_file('./samples/gpl-v2')
+        licenses = attempt2.load_database()
+        result = attempt2.guess_license(license, licenses)
+        self.assertEqual('GPL-2.0', result[0][1]['shortname'])
 
 if __name__ == "__main__":
     unittest.main()
