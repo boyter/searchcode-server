3293c94b27ea8924f0e163b6c5d6235755946b4c
Ben Boyter <ben@boyter.org>
2017-09-21 17:54:28 +1000

    Various fixes

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.10
+ * Version 1.3.11
  */
 '''
 
@@ -98,6 +98,8 @@ def build_release():
     }
     _python_sed(
         fileloc='./src/main/java/com/searchcode/app/App.java', replacements=replacements)
+    local('rm -rf ./repo/')
+    local('rm -rf ./index/')
     compile_js()
     configure_prod()
     package()
