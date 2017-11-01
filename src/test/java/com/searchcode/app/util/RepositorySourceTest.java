d88122e8abd026b7cd60d44a65ea871d0fc57a9b
Ben Boyter <ben@boyter.org>
2017-11-02 08:43:38 +1100

    Tweak creation somewhat

--- src/test/java/com/searchcode/app/util/RepositorySourceTest.java
+++ src/test/java/com/searchcode/app/util/RepositorySourceTest.java
@@ -3,6 +3,8 @@ package com.searchcode.app.util;
 import com.searchcode.app.dto.Source;
 import junit.framework.TestCase;
 
+import java.util.HashMap;
+
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class RepositorySourceTest extends TestCase {
@@ -18,5 +20,17 @@ public class RepositorySourceTest extends TestCase {
         RepositorySource repositorySource = new RepositorySource();
         assertThat(repositorySource.getSourceByName("GitHub").get().getName()).isEqualTo("GitHub");
         assertThat(repositorySource.getSourceByName("HOPEFULLYNEVEREXISTS").isPresent()).isFalse();
+    }
+
+    public void testGetLink() {
+        RepositorySource repositorySource = new RepositorySource();
+        String link = repositorySource.getLink("GitHub", new HashMap<String, String>() {{
+            put("user", "boyter");
+            put("project", "searchcode-server");
+            put("branch", "master");
+            put("filepath", "fabfile.py");
+        }});
+
+        assertThat(link).isEqualTo("https://github.com/boyter/searchcode-server/blob/master/fabfile.py");
     }
 }
