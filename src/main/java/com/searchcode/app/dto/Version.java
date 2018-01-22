dca62869e44f8c932f9ea5be1fc43889508b4610
Ben Boyter <ben@boyter.org>
2018-01-22 13:11:45 +1100

    Move version over to use searchcodeserver.com and update background images

---
+++ src/main/java/com/searchcode/app/dto/Version.java
@@ -0,0 +1,13 @@
+package com.searchcode.app.dto;
+
+public class Version {
+    public final String version;
+
+    public Version(String version) {
+        this.version = version;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+}
