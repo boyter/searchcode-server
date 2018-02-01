1a97f0674a6e864ae605faf43a1f44a08900f9f9
Ben Boyter <ben@boyter.org>
2018-02-01 19:58:02 +1100

    Start version 1.3.13

--- src/main/java/com/searchcode/app/App.java
+++ src/main/java/com/searchcode/app/App.java
@@ -5,7 +5,7 @@
  * in the LICENSE.TXT file, but will be eventually open under GNU General Public License Version 3
  * see the README.md for when this clause will take effect
  *
- * Version 1.3.12
+ * Version 1.3.13
  */
 
 package com.searchcode.app;
@@ -36,8 +36,8 @@ import static spark.Spark.*;
  */
 public class App {
 
-    public static final boolean ISCOMMUNITY = false;
-    public static final String VERSION = "1.3.12";
+    public static final boolean ISCOMMUNITY = true;
+    public static final String VERSION = "1.3.13";
 
     public static void main(String[] args) {
         // Database migrations happen before we start
@@ -224,7 +224,7 @@ public class App {
                 halt();
                 return null;
             }
-            
+
             Map<String, Object> map = new HashMap<>();
             map.put("logoImage", CommonRouteService.getLogo());
             map.put("isCommunity", ISCOMMUNITY);
