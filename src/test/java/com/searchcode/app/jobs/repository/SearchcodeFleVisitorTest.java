edd8de352b9e12a5b9c800102b474a3489a84bcf
Ben Boyter <ben@boyter.org>
2018-03-02 08:19:11 +1100

    Test now runs all paths of the walk tree

--- src/test/java/com/searchcode/app/jobs/repository/SearchcodeFleVisitorTest.java
+++ src/test/java/com/searchcode/app/jobs/repository/SearchcodeFleVisitorTest.java
@@ -2,6 +2,7 @@ package com.searchcode.app.jobs.repository;
 
 import com.searchcode.app.dao.Data;
 import com.searchcode.app.dto.CodeLinesReturn;
+import com.searchcode.app.dto.IsMinifiedReturn;
 import com.searchcode.app.dto.RepoData;
 import com.searchcode.app.model.RepoResult;
 import com.searchcode.app.service.Singleton;
@@ -14,7 +15,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.Matchers.any;
@@ -28,11 +31,14 @@ public class SearchcodeFleVisitorTest extends TestCase {
         RepoResult repoResult = mock(RepoResult.class);
         RepoData repoData = new RepoData();
         repoData.jobRunTime = Instant.MIN;
+
         when(repoResult.getData()).thenReturn(repoData);
-        when(mockIndexBase.getCodeLines(any(), any())).thenReturn(new CodeLinesReturn(null, null, false));
+
+        when(mockIndexBase.getCodeLines(any(), any())).thenReturn(new CodeLinesReturn(new ArrayList<String>(), null, false));
+        when(mockIndexBase.getIsMinified(any(), any(), any())).thenReturn(new IsMinifiedReturn(false, null));
+        when(mockIndexBase.getFileLocationFilename(any(), any())).thenReturn("");
 
         SearchcodeFileVisitor<Path> searchcodeFileVisitor = new SearchcodeFileVisitor<>(mockIndexBase, repoResult, "./", "");
         Files.walkFileTree( Paths.get("./"), EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, searchcodeFileVisitor);
-        assertThat(true).isFalse();
     }
 }
