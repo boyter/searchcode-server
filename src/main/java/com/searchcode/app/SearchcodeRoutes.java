05dbe15ba53fbca519b5a77d20a1c2c7b820c8ad
Ben Boyter <ben@boyter.org>
2018-10-05 08:31:14 +1000

    Remove gzip header for sphinx index as should be behind nginx anyway

--- src/main/java/com/searchcode/app/SearchcodeRoutes.java
+++ src/main/java/com/searchcode/app/SearchcodeRoutes.java
@@ -10,24 +10,20 @@ import static spark.Spark.get;
 public class SearchcodeRoutes {
     public static void RegisterSearchcodeRoutes() {
         get("/", (request, response) -> {
-            response.header("Content-Encoding", "gzip");
             CodeRouteService codeRouteService = new CodeRouteService();
             return new FreeMarkerEngine().render(codeRouteService.html(request, response));
         });
 
         get("/healthcheck/", (request, response) -> {
-            response.header("Content-Encoding", "gzip");
             return new JsonTransformer().render(true);
         });
 
         get("/file/:codeid/*", (request, response) -> {
-            response.header("Content-Encoding", "gzip");
             CodeRouteService codeRouteService = new CodeRouteService();
             return new FreeMarkerEngine().render(new ModelAndView(codeRouteService.getCode(request, response), "coderesult.ftl"));
         });
 
         get("/repository/overview/:reponame/", (request, response) -> {
-            response.header("Content-Encoding", "gzip");
             CodeRouteService codeRouteService = new CodeRouteService();
             return new FreeMarkerEngine().render(new ModelAndView(codeRouteService.getProject(request, response), "repository_overview.ftl"));
         });
