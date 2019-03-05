4a7653b6a1ac3eb27d339b8107aa268f6eb6a381
Ben Boyter <ben@boyter.org>
2019-03-06 08:32:00 +1100

    Tweak slightly

--- src/test/java/com/searchcode/app/AppTest.java
+++ src/test/java/com/searchcode/app/AppTest.java
@@ -3,15 +3,12 @@ package com.searchcode.app;
 import junit.framework.TestCase;
 import org.mindrot.jbcrypt.BCrypt;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 public class AppTest extends TestCase {
     public void testCrypt() {
-        String hash = BCrypt.hashpw("password", BCrypt.gensalt());
+        var hash = BCrypt.hashpw("password", BCrypt.gensalt());
 
-        if (BCrypt.checkpw("password", hash)) {
-            // Login successful.
-            assertTrue(true);
-        } else {
-            assertTrue(false);
-        }
+        assertThat(BCrypt.checkpw("password", hash)).isTrue();
     }
 }
