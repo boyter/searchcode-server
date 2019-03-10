78afae1709e36acb88f619187a5c718edd6f93be
Ben Boyter <ben@boyter.org>
2019-03-11 08:46:01 +1100

    Return pooling connection test

--- src/test/java/com/searchcode/app/config/MySQLDatabaseConfigTest.java
+++ src/test/java/com/searchcode/app/config/MySQLDatabaseConfigTest.java
@@ -13,4 +13,18 @@ public class MySQLDatabaseConfigTest extends TestCase {
         var connection = mySQLDatabaseConfig.getConnection();
         connection.close();
     }
+
+    /**
+     * Ensure that connection pooling is working as expected
+     */
+    public void testManyConnection() throws SQLException {
+        if (Singleton.getHelpers().isStandaloneInstance()) return;
+
+        var mySQLDatabaseConfig = Singleton.getDatabaseConfig();
+
+        for (var i=0; i< 10000; i++) {
+            var connection = mySQLDatabaseConfig.getConnection();
+            connection.close();
+        }
+    }
 }
