b9c7eb09df81e029bcb5afa05ffec0d8800ff0ae
Ben Boyter <ben@boyter.org>
2019-04-11 08:56:52 +1000

    Start to add public api

--- src/main/java/com/searchcode/app/SearchcodeRoutes.java
+++ src/main/java/com/searchcode/app/SearchcodeRoutes.java
@@ -6,6 +6,7 @@ import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 import static spark.Spark.get;
+import static spark.Spark.path;
 
 /**
  * Provides all the routes for the searchcode.com version of searchcode
@@ -42,6 +43,26 @@ public class SearchcodeRoutes {
             return new FreeMarkerEngine().render(
                     new ModelAndView(codeRouteService.getProject(request, response),
                             "repository_overview.ftl"));
+        });
+
+
+        ///api/codesearch_I/
+        path("/api", () -> {
+            // Older endpoints maintained for backwards compatibility
+            get("/codesearch_I/", (request, response) -> {
+               return new JsonTransformer().render(null);
+            });
+
+            // All new API endpoints should go in here to allow public exposure and versioning
+            path("/v1", () -> {
+                get("/version/", (request, response) -> {
+                    return new JsonTransformer().render("");
+                });
+
+                get("/health-check/", (request, response) -> {
+                    return new JsonTransformer().render("");
+                });
+            });
         });
     }
 }
